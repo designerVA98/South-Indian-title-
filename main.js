21d1bf1141fca0bdd42df94f3e1fb30577f6bd35
designerVA98 <vancreativity98@gmail.com>
2026-03-20 13:42:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,35 @@ text.split("").forEach((char, i) => {
   span.style.animationDelay = `${i * 0.1}s`;
   title.appendChild(span);
 });
+/* SECOND TITLE TEXT */
+const text2 = "DAYLIGHT";
+const subtitleText = "Story by Valaboju Akhil";
+
+const title2 = document.getElementById("title2");
+const subtitle2 = document.getElementById("subtitle2");
+
+/* Scroll trigger */
+window.addEventListener("scroll", () => {
+  const section = document.querySelector(".section2");
+  const position = section.getBoundingClientRect().top;
+
+  if (position < window.innerHeight - 100) {
+    section.classList.add("active");
+
+    /* Title animation */
+    if (title2.innerHTML === "") {
+      text2.split("").forEach((char, i) => {
+        const span = document.createElement("span");
+        span.textContent = char;
+        span.classList.add("letter2");
+        span.style.animationDelay = `${i * 0.1}s`;
+        title2.appendChild(span);
+      });
+
+      /* Subtitle delay */
+      setTimeout(() => {
+        subtitle2.innerText = subtitleText;
+      }, 1200);
+    }
+  }
+});
